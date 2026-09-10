3e807fc3729c77ef25757bc683d6f3f950db60d2
Maxwell Dantas <144301678+maxwell-dantas@users.noreply.github.com>
2026-09-10 01:34:57 -0300

    refactor: modicação de diretório e nomenclatura da interface

--- src/vector/VectorADT.java
+++ src/vector/VectorADT.java
@@ -1,6 +1,6 @@
-package vector.array;
+package vector;
 
-public interface VectorArrayADT {
+public interface VectorADT {
     public Object elementAtRank(Integer r);
     public Object replaceAtRank(Integer r, Object item);
     public void insertAtRank(Integer r, Object item);
